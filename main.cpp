8129bfbde3171f4315a7899dafb650af1c79795e
no <insafyusupov23@gmail.com>
2021-08-04 00:05:04 +0300

    Updating files

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-#include
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    vector<int> array = {28, 19, 42, 53, 69};
+    double m;
+    for (int i : array) {
+        m += i;
+
+    }
+    m /= array.size();
+    double u=0;
+    for (int i : array) {
+        u += pow(i - m, 2);
+    }
+    u = sqrt(u / array.size());
+    cout << m << " " << u;
+}
